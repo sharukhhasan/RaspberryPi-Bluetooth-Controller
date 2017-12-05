9c46a5ce1b64f8e8327b80424a78d0eb763f9017
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2017-12-04 21:47:11 -0600

    adding gatt service script

---
+++ gatt-service.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+usage = """Usage:
+python gatt-service.py &
+python gatt-client.py
+python gatt-async-client.py
+python gatt-client.py --exit-service
+"""
+
+import gobject
+
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+
+class DemoException(dbus.DBusException):
+    _dbus_error_name = 'com.example.DemoException'
+
+class SomeObject(dbus.service.Object):
+
+    @dbus.service.method("com.example.SampleInterface",
+                         in_signature='s', out_signature='as')
+    def HelloWorld(self, hello_message):
+        print (str(hello_message))
+        return ["Hello", " from gatt-service.py", "with unique name",
+                session_bus.get_unique_name()]
+
+    @dbus.service.method("com.example.SampleInterface",
+                         in_signature='', out_signature='')
+    def RaiseException(self):
+        raise DemoException('The RaiseException method does what you might '
+                            'expect')
+
+    @dbus.service.method("com.example.SampleInterface",
+                         in_signature='', out_signature='(ss)')
+    def GetTuple(self):
+        return ("Hello Tuple", " from gatt-service.py")
+
+    @dbus.service.method("com.example.SampleInterface",
+                         in_signature='', out_signature='a{ss}')
+    def GetDict(self):
+        return {"first": "Hello Dict", "second": " from gatt-service.py"}
+
+    @dbus.service.method("com.example.SampleInterface",
+                         in_signature='', out_signature='')
+    def Exit(self):
+        mainloop.quit()
+
+
+if __name__ == '__main__':
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    session_bus = dbus.SessionBus()
+    name = dbus.service.BusName("com.example.SampleService", session_bus)
+    object = SomeObject(session_bus, '/SomeObject')
+
+    mainloop = gobject.MainLoop()
+    print "Running example service."
+    print usage
+    mainloop.run()
